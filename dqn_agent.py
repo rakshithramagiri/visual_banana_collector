256622becbd8dde496fc47d4e7e24d34620a4907
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-03-16 18:02:46 +0530

    add: added dqn_agent.py file to store agent definition

---
+++ dqn_agent.py
@@ -0,0 +1,44 @@
+import torch
+import numpy as np
+import random
+
+##################################
+# Global Variables
+
+
+
+##################################
+
+
+class DQNAgent:
+    def __init__(self):
+        pass
+
+    def act(self):
+        pass
+
+
+    def learn(self):
+        pass
+
+
+    def target_network_update(self):
+        pass
+
+
+
+
+
+
+class ReplayMemory:
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def sample(self):
+        pass
+
+    def __len__(self):
+        return len(self.memory)
